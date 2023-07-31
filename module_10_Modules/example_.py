b18c6595c721e3f223a4ef700c559d259ef15e4a
vikramviky123 <58477512+vikramviky123@users.noreply.github.com>
2023-07-31 22:25:36 +0530

    Module_10

---
+++ module_10_Modules/example_.py
@@ -0,0 +1,6 @@
+def print_name(name):
+    """
+    This function does not return anything.
+    It will just print your name.
+    """
+    print("Hello! {}, You are in example module.".format(name))
---
+++ module_10_Modules/test_.py
@@ -0,0 +1,4 @@
+import example_
+
+name = "scaler academy"
+example_.print_name(name)
